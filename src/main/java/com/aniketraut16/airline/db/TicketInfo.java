260d3cd55f0624f46b8118f550f0de8912a9730a
aniketraut16 <rautan_1@rknec.edu>
2025-03-21 20:35:48 +0530

    Add FlightInfo, ScheduleInfo, and TicketInfo data models

---
+++ src/main/java/com/aniketraut16/airline/db/TicketInfo.java
@@ -0,0 +1,15 @@
+package com.aniketraut16.airline.db;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Builder(toBuilder = true)
+public class TicketInfo {
+    private Long id;
+    private Long flightId;
+    private LocalDate travelDate;
+    private String passengerName;
+    private String bookingReference;
+}
